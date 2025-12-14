4f1420068015f1fa4405130304065c4b0d62c81a
Anton Vakhrushev <btbpanda@gmail.com>
2025-12-15 01:53:22 +0400

    Update model implementation

--- protlib/metric.py
+++ protlib/metric.py
@@ -311,17 +311,17 @@ class CAFAMetric:
         merged['bin_x'] = merged['bin'] + 1
         mtoi = merged.query('flg')
 
-        print(merged.columns)
-        print(mtoi.columns)
-        print(mtoi['entry_id'].nunique() - mtoi \
-            .groupby('entry_id')['bin_x'].max() \
-            .value_counts() \
-            .to_frame() \
-            .join(cudf.DataFrame([], index=cudf.RangeIndex(n_bins)), how='right') \
-            .sort_index() \
-            .fillna(0) \
-            .cumsum()
-        )
+        # print(merged.columns)
+        # print(mtoi.columns)
+        # print(mtoi['entry_id'].nunique() - mtoi \
+        #     .groupby('entry_id')['bin_x'].max() \
+        #     .value_counts() \
+        #     .to_frame() \
+        #     .join(cudf.DataFrame([], index=cudf.RangeIndex(n_bins)), how='right') \
+        #     .sort_index() \
+        #     .fillna(0) \
+        #     .cumsum()
+        # )
 
         # cov = mtoi['entry_id'].nunique() - mtoi \
         #     .groupby('entry_id')['bin_x'].max() \
