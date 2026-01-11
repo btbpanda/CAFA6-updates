1720fb389e7bd9e0b14cfa8a23a1cbde8b7373a6
Anton Vakhrushev <btbpanda@gmail.com>
2026-01-11 22:15:20 +0400

    Add postproc pl

---
+++ protlib/scripts/postproc/avg_preds.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+import sys
+
+import tqdm
+import polars as pl
+
+sys.path.append(os.path.abspath(os.path.join(__file__, '../../../../')))
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-imax', '--input-max', type=str)
+parser.add_argument('-imin', '--input-min', type=str)
+parser.add_argument('-o', '--output-path', type=str)
+
+parser.add_argument('-d', '--device', type=str, default="1")
+
+
+def write_csv_batch(df, file, mode='w', batch_size=10000000, **kwargs):
+    for i in tqdm.tqdm(range(0, len(df), batch_size)):
+        batch = df[i: i + batch_size]
+
+        with open(file, mode) as f:
+            batch.to_csv(f, **kwargs)
+            mode = 'a'
+
+    return
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.device
+    import cupy as cp
+    import cudf
+
+    try:
+        from protlib.metric import get_funcs_mapper, get_ns_id, obo_parser, Graph
+    except Exception:
+        get_funcs_mapper, get_ns_id, obo_parser, Graph = [None] * 4
+
+    with pl.StringCache():
+
+        pred_max = pl.read_csv(
+            args.input_max, has_header=False, separator='\t', new_columns=['EntryID', 'term', 'p'],
+            schema={'EntryID': pl.Categorical, 'term': pl.Categorical, 'p': pl.Float32}
+        )
+
+        pred_min = pl.read_csv(
+            args.input_min, has_header=False, separator='\t', new_columns=['EntryID', 'term', 'p'],
+            schema={'EntryID': pl.Categorical, 'term': pl.Categorical, 'p': pl.Float32}
+        )
+
+    pred = cudf.from_pandas(
+        pl.concat([pred_max, pred_min]).to_pandas()
+    )
+
+    pred = pred.groupby(['EntryID', 'term'])['p'].mean().reset_index().sort_values(['EntryID', 'p'], ascending=False)
+
+    write_csv_batch(
+        pred, args.output_path, 
+        mode='w', batch_size=10000000,
+        sep='\t', header=False, index=False
+    )
