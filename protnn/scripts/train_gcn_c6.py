97a78354cfefc763758ebf57c8ded778136a20be
Anton Vakhrushev <btbpanda@gmail.com>
2026-01-18 15:12:46 +0400

    Add postproc pl

--- protnn/scripts/train_gcn_c6.py
+++ protnn/scripts/train_gcn_c6.py
@@ -280,8 +280,8 @@ if __name__ == '__main__':
     )
 
     # test evaluation
-    score = evaluator(model, val_dl)
-    print('Test score value: ', score)
+    # score = evaluator(model, val_dl)
+    # print('Test score value: ', score)
 
     ############################################
     # FIT AND SAVE
