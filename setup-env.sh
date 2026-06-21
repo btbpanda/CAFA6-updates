b2f46f43a80ec024ecc13f11b7ae60ee1166c6ce
Anton Vakhrushev <btbpanda@gmail.com>
2026-06-21 18:07:58 +0400

    Reduce proteins + update env

--- setup-env.sh
+++ setup-env.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-conda create -n rapids-25.12 -c rapidsai -c conda-forge \
-    cudf=25.12 python=3.12 'cuda-version>=12.2,<=12.9' \
+#conda create -n rapids-25.12 -c rapidsai -c conda-forge \
+#    cudf=25.12 python=3.12 'cuda-version>=12.2,<=12.9' \
+#    'pytorch=*=*cuda*' --solver=libmamba
+
+conda create -n rapids-26.06 -c rapidsai -c conda-forge \
+    rapids=26.06 python=3.12 'cuda-version>=12.2,<=12.9' \
     'pytorch=*=*cuda*' --solver=libmamba
+
 
 pip install -r requirements.txt
