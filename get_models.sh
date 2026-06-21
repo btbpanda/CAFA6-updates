fc7192813fea76bafc800512d18f0a2cfb31be66
Anton Vakhrushev <btbpanda@gmail.com>
2026-06-22 00:54:35 +0400

    Fix typos in get_models.sh

--- get_models.sh
+++ get_models.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Inference DL embeddings
-python ./model_scipts/run_embeddings_inference.py
+python ./model_scripts/run_embeddings_inference.py
 
 # run base models
-python ./model_scipts/run_pb_and_lin.py
+python ./model_scripts/run_pb_and_lin.py
 
 # aggregate base models
-python ./model_scipts/model_aggregator.py
+python ./model_scripts/model_aggregator.py
 
 # aggregate base models
-python ./model_scipts/model_aggregator.py
+python ./model_scripts/model_aggregator.py
 
 # stacker fit_predict
-python ./run_stacker/run_stacker.py
+python ./model_scripts/run_stacker.py
 
 # postprocess and make submission
-python ./run_stacker/postproc_and_sub.py
+python ./model_scripts/postproc_and_sub.py
