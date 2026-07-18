091b3a998ef586c00db435623b0d510c19b5a258
Anton Vakhrushev <btbpanda@gmail.com>
2026-07-18 15:43:02 +0400

    Remove duplicated aggregation (already in main)

--- get_models.sh
+++ get_models.sh
@@ -9,9 +9,6 @@ python ./model_scripts/run_pb_and_lin.py
 # aggregate base models
 python ./model_scripts/model_aggregator.py
 
-# aggregate base models
-python ./model_scripts/model_aggregator.py
-
 # stacker fit_predict
 python ./model_scripts/run_stacker.py
 
