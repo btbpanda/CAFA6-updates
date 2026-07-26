e57495b1df050ccf50b85a3f8d649c92c4b3ebf0
Anton Vakhrushev <btbpanda@gmail.com>
2026-07-26 23:55:57 +0400

    Fix config with docker paths and remove mount of data

--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -4,6 +4,5 @@ docker run --gpus all \
     --shm-size=8g \
     --ipc=host \
     -v $(pwd)/kaggle.json:/root/.kaggle/kaggle.json \
-    -v $(pwd)/data:/workspace/data \
     -it --rm \
     cafa6-solution
